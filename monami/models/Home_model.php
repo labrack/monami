e2824a2d2f9845eed225ebd675ac22639a827d4e
Tom Duff <labrack@users.noreply.github.com>
2016-10-15 00:14:56 +0900

    Add get_number_of_checks to Home (10F Menu) model

--- monami/models/Home_model.php
+++ monami/models/Home_model.php
@@ -33,5 +33,19 @@ class Home_model extends CI_Model {
 			return false;
 		}
 	}
+	function get_number_of_checks($userid){
+		$sql = "Select count(ID) as count FROM tblMonitoringChecks where HostID in (select ID from tblMonitoringHosts where OwnerID in (select ID from tblMonitoringOwners where OwnerMasterUserID = $userid))";
+		$query = $this->db->query($sql);
+		if($query->num_rows() >= 1){
+			$retval = '';
+			foreach($query->result() as $row){
+				$retval .= "$row->count";
+			}
+			return $retval;
+		}
+		else{
+			return false;
+		}
+	}
 }
 ?>
